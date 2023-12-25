e2bf96fa2509d6c8c4cd7c9f4f584614cb5473ae
wcg <1721097664@qq,com>
2023-12-25 21:41:00 +0800

    third commit

--- wcg.cpp
+++ wcg.cpp
@@ -1,8 +1,7 @@
-#include <iostream>
-
-int main()
-{
-	int a = 0;
-	std::cout<<"hello world!"<<std::endl;
-	return 0;
-}
+echo "# test" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/fzdzz/test.git
+git push -u origin main
